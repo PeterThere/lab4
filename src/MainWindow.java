a0916b752bea8aaa18328e01086f99f5e42fdf1d
PeterThere <72750867+PeterThere@users.noreply.github.com>
2021-12-13 19:50:28 +0100

    Uciekający przycisk + kanwa

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Random;
@@ -17,14 +19,11 @@ public class MainWindow extends JFrame{
             }
         });
     }
-    public MainWindow() throws HeadlessException {
-        this("undefined");
-    }
 
     public MainWindow(String title) throws HeadlessException {
         super(title);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize( 720, 480);
+        setSize( 1020, 480);
         setLocationRelativeTo(null);
 
         JPanel panel = new JPanel();
@@ -32,32 +31,53 @@ public class MainWindow extends JFrame{
         setContentPane(panel);
         panel.setLayout(null);
 
-            Random random = new Random();
+        Random random = new Random();
 
-            JButton btnRun = new JButton("Złap tu →");
-            btnRun.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    super.mouseEntered(e);
-                    Point p = e.getLocationOnScreen();
-                    SwingUtilities.convertPointFromScreen(p, panel);
-                    if(p.x < (btnRun.getX()+90)) //trzeba najechać z prawej strony, aby przycisk nie zmienił pozycji
+        JButton btnRun = new JButton("Złap tu →");
+        btnRun.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+                Point p = e.getLocationOnScreen();
+                SwingUtilities.convertPointFromScreen(p, panel);
+                if (p.x < (btnRun.getX() + 90)) //trzeba najechać z prawej strony, aby przycisk nie zmienił pozycji
                     btnRun.setLocation(random.nextInt(600), random.nextInt(300));
-                }
-//                @Override
-//                public void mousePressed(MouseEvent e) {
-//                    super.mousePressed(e);
-//                    MainWindow etap2 = new MainWindow("Kanwa");
-//                    etap2.setVisible(true);
-//                }
-            });
-            btnRun.setBounds(300, 180, 100, 50);
-            panel.add(btnRun);
+            }
+        });
+        btnRun.setBounds(300, 180, 100, 50);
+        panel.add(btnRun);
 
-            JButton btnReset = new JButton("RESET");
-            btnReset.addActionListener(e -> btnRun.setLocation(300, 180));
-            btnReset.setBounds(300, 380, 100, 50);
-            panel.add(btnReset);
+        JButton btnReset = new JButton("RESET");
+        btnReset.addActionListener(e -> btnRun.setLocation(300, 180));
+        btnReset.setBounds(300, 380, 100, 50);
+        panel.add(btnReset);
+
+        JPanel kanwa = new JPanel();
+        kanwa.setBounds(700,0,320,460);
+        kanwa.setBackground(Color.gray);
+        kanwa.setFocusable(true);
+        panel.add(kanwa);
+
+        JPanel kwadrat = new JPanel();
+        kwadrat.setLayout(null);
+        kanwa.add(kwadrat);
+
+        kanwa.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                Point p = MouseInfo.getPointerInfo().getLocation();
+                SwingUtilities.convertPointFromScreen(p, kanwa);
+                if(e.getKeyChar() == KeyEvent.VK_1){
+                    kwadrat.setBounds(p.x, p.y, 50, 50);
+                    kwadrat.setBackground(Color.cyan);
+                }
+                if(e.getKeyChar() == KeyEvent.VK_2){
+                    kwadrat.setBounds(p.x, p.y, 100, 50);
+                    kwadrat.setBackground(Color.magenta);
+                }
+            }
+        });
     }
 
 }
